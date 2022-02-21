2ee0cecb27dabb0c28bc0b006394a29cbeae3768
Adeeb Siddiqui <adeeb.bd@gmail.com>
2022-02-20 23:43:50 -0600

    Remove all verbose comments

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,20 @@
+import { useState } from 'react'; //import with {} are named imports which means only import specific function from library
+// 'useState' hook is used for managing state of React components. Read README
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+
+import './ExpenseForm.css';
+
+const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+  const titleChangeHandler = (event) => { //'event' is a default JS object provided by the browser whenever we listen to events on HTML elements
+    setEnteredTitle(event.target.value);
+  };
+
+  const amountChangeHandler = event => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = event => {
+    setEnteredDate(event.target.value);
+  };
+
+  const submitHandler = (event) => { // 'submit event' emitted by form
+    event.preventDefault(); //Default browser behavior: whenever a form is submitted, browser sends a request to the server and reloads the page
+    // The above vanilla JS statement prevents that default browser behavior
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    props.onSaveExpenseData(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+  };
+
+  return (
+    <form onSubmit={submitHandler}> {/*Default browser behavior: if a submit button is pressed inside of a form then the form itself will emit a 'submit event' which we can listen*/}
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label>Title</label>
+          <input
+            type='text'
+            value={enteredTitle} //2-Way binding
+            onChange={titleChangeHandler}
+          />
+        </div>
+        <div className='new-expense__control'>
+          <label>Amount</label>
+          <input
+            type='number'
+            min='0.01'
+            step='0.01'
+            value={enteredAmount} //2-Way binding
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div className='new-expense__control'>
+          <label>Date</label>
+          <input
+            type='date'
+            min='2019-01-01'
+            max='2022-12-31'
+            value={enteredDate} //2-Way binding
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className='new-expense__actions'>
+        <button type='submit'>Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
