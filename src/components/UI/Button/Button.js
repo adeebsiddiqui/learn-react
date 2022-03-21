75e7992c04cc9bbf721091fe76cd0dfd50b70e9e
Adeeb Siddiqui <adeeb.bd@gmail.com>
2022-03-21 02:03:56 -0500

    Dynamic styles with CSS Modules & Media Queries

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-import './Button.css';
+import styles from './Button.module.css'; //example of CSS Module. The object name 'styles' here is arbitrary.
+// More about CSS module - https://create-react-app.dev/docs/adding-a-css-modules-stylesheet/
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
