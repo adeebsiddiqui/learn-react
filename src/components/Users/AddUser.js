a2d65dfee72072d752345a27a524bb27bf1e82f9
Adeeb Siddiqui <adeeb.bd@gmail.com>
2023-10-20 00:11:26 -0500

    Introducing React Refs

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, Fragment, useRef } from 'react'; //To use Refs we have to import the 'useRef' hook
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
@@ -6,37 +6,36 @@ import ErrorModal from '../UI/ErrorModal';
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+  const nameInputRef = useRef();  //Refs allow us to get access to other DOM elements, here nameInputRef has access to 'input' element at line 53
+  const ageInputRef = useRef(); //ageInputRef has access to 'input' element at line 55
+
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+
+    const enteredName = nameInputRef.current.value; //holds the current value of 'input' element at line 53
+    const enteredUserAge = ageInputRef.current.value; //holds the current value of 'input' element at line 55
+    
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
         title: 'Invalid input',
         message: 'Please enter a valid name and age (non-empty values).',
       });
       return;
     }
-    if (+enteredAge < 1) {
+
+    if (+enteredUserAge < 1) {
       setError({
         title: 'Invalid age',
         message: 'Please enter a valid age (> 0).',
       });
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername('');
-    setEnteredAge('');
-  };
 
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
-
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
+    props.onAddUser(enteredName, enteredUserAge);
+    nameInputRef.current.value = ''; //Refs shouldn't be used to manipulate the DOM e.g. add a new element or change a CSS class
+    ageInputRef.current.value = '';  // These two Refs are just changing the value of 'input' element, not manipulating the DOM hence its okay
   };
 
   const errorHandler = () => {
@@ -44,7 +43,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <Fragment> {/* Using React Fragment to avoid <div> Soup */}
+    <Fragment>
       {error && (
         <ErrorModal
           title={error.title}
@@ -55,19 +54,9 @@ const AddUser = (props) => {
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
-          <input
-            id="username"
-            type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
-          />
+          <input id="username" type="text" ref={nameInputRef} /> {/* 'ref' is a built in React prop that can be added to any HTML element */}
           <label htmlFor="age">Age (Years)</label>
-          <input
-            id="age"
-            type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
-          />
+          <input id="age" type="number" ref={ageInputRef} />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
